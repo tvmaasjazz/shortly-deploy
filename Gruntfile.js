91889886cb1fddece92cdde4053083d633020842
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-09-15 15:12:18 -0700

    Get rid of grunt to see if cam deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,119 +1,119 @@
-const grunt = require('grunt');
+// const grunt = require('grunt');
 
-module.exports = function(grunt) {
+// module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';',
-      },
-      dist: {
-        src: ['public/client/**/*.js'],
-        dest: 'public/dist/bundle.js',
-      }
-    },
+//   grunt.initConfig({
+//     pkg: grunt.file.readJSON('package.json'),
+//     concat: {
+//       options: {
+//         separator: ';',
+//       },
+//       dist: {
+//         src: ['public/client/**/*.js'],
+//         dest: 'public/dist/bundle.js',
+//       }
+//     },
 
-    clean: ['public/dist/bundle.js'],
+//     clean: ['public/dist/bundle.js'],
 
-    launch: {
-      command: 'git push heroku master'
-    }
+//     launch: {
+//       command: 'git push heroku master'
+//     }
 
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
-        },
-        src: ['test/**/*.js']
-      }
-    },
+//     mochaTest: {
+//       test: {
+//         options: {
+//           reporter: 'spec'
+//         },
+//         src: ['test/**/*.js']
+//       }
+//     },
 
-    nodemon: {
-      dev: {
-        script: 'server.js'
-      }
-    },
+//     nodemon: {
+//       dev: {
+//         script: 'server.js'
+//       }
+//     },
 
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'public/dist/bundle.js',
-        dest: 'public/dist/bundle.min.js'
-      }
-    },
+//     uglify: {
+//       options: {
+//         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+//       },
+//       build: {
+//         src: 'public/dist/bundle.js',
+//         dest: 'public/dist/bundle.min.js'
+//       }
+//     },
 
-    eslint: {
-      target: [
-        // Add list of files to lint here
-      ]
-    },
+//     eslint: {
+//       target: [
+//         // Add list of files to lint here
+//       ]
+//     },
 
-    cssmin: {
-    },
+//     cssmin: {
+//     },
 
-    watch: {
-      scripts: {
-        files: [
-          'public/client/**/*.js',
-          'public/lib/**/*.js',
-        ],
-        tasks: [
-          'concat',
-          'uglify'
-        ]
-      },
-      css: {
-        files: 'public/*.css',
-        tasks: ['cssmin']
-      }
-    },
+//     watch: {
+//       scripts: {
+//         files: [
+//           'public/client/**/*.js',
+//           'public/lib/**/*.js',
+//         ],
+//         tasks: [
+//           'concat',
+//           'uglify'
+//         ]
+//       },
+//       css: {
+//         files: 'public/*.css',
+//         tasks: ['cssmin']
+//       }
+//     },
 
-    shell: {
-      prodServer: {
-      }
-    },
-  });
+//     shell: {
+//       prodServer: {
+//       }
+//     },
+//   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-nodemon');
+//   grunt.loadNpmTasks('grunt-contrib-uglify');
+//   grunt.loadNpmTasks('grunt-contrib-watch');
+//   grunt.loadNpmTasks('grunt-contrib-concat');
+//   grunt.loadNpmTasks('grunt-contrib-clean');
+//   grunt.loadNpmTasks('grunt-contrib-cssmin');
+//   grunt.loadNpmTasks('grunt-eslint');
+//   grunt.loadNpmTasks('grunt-mocha-test');
+//   grunt.loadNpmTasks('grunt-shell');
+//   grunt.loadNpmTasks('grunt-nodemon');
 
-  grunt.registerTask('server-dev', function (target) {
-    grunt.task.run([ 'nodemon', 'watch' ]);
-  });
+//   grunt.registerTask('server-dev', function (target) {
+//     grunt.task.run([ 'nodemon', 'watch' ]);
+//   });
 
-  ////////////////////////////////////////////////////
-  // Main grunt tasks
-  ////////////////////////////////////////////////////
+//   ////////////////////////////////////////////////////
+//   // Main grunt tasks
+//   ////////////////////////////////////////////////////
 
-  grunt.registerTask('test', [
-    'mochaTest'
-  ]);
+//   grunt.registerTask('test', [
+//     'mochaTest'
+//   ]);
 
-  grunt.registerTask('build', [
-    'clean', 'concat', 'uglify'
-  ]);
+//   grunt.registerTask('build', [
+//     'clean', 'concat', 'uglify'
+//   ]);
 
-  grunt.registerTask('upload', function(n) {
-    if (grunt.option('prod')) {
-      // add your production server task here
-    } else {
-      grunt.task.run([ 'server-dev' ]);
-    }
-  });
+//   grunt.registerTask('upload', function(n) {
+//     if (grunt.option('prod')) {
+//       // add your production server task here
+//     } else {
+//       grunt.task.run([ 'server-dev' ]);
+//     }
+//   });
 
-  grunt.registerTask('deploy', [
-    'launch'
-  ]);
+//   grunt.registerTask('deploy', [
+//     'launch'
+//   ]);
 
 
-};
+// };
